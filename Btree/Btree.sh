5564367983327e31fe3318fd182ba3e37a1372f7
Nhat-Thanh <luonkebenban@gmail.com>
2021-05-10 17:43:43 +0700

    Main shell script file to run all operation

--- Btree/Btree.sh
+++ Btree/Btree.sh
@@ -1,43 +1,43 @@
-operation=$1
+OPERATION=$1
 
-if [ $operation != "insert" -a \
-	$operation != "delete" -a \
-	$operation != "rank" -a \
-	$operation != "between" -a \
-	$operation != "update" ]
+if [ $OPERATION != "insert" -a \
+	$OPERATION != "delete" -a \
+	$OPERATION != "rank" -a \
+	$OPERATION != "between" -a \
+	$OPERATION != "update" ]
 then
 	echo "operation is invalid
 	insert, delete, rank, between, update are acceptable"
 	exit 0
 fi
 # INSERT OPERATION
-if [ $operation == "insert" ]
+if [ $OPERATION == "insert" ]
 then
 	/usr/bin/time -v -o ./result/insert/insert_operation_time.txt ./operating_script/insert.sh | \
-		./measure_ram.sh $operation
+		./measure_ram.sh $OPERATION
 
 # DELETE OPERATION
-elif [ $operation == "delete" ]
+elif [ $OPERATION == "delete" ]
 then
 	/usr/bin/time -v -o "./result/delete/delete_operation_time.txt" ./operating_script/delete.sh | \
-		./measure_ram.sh $operation
+		./measure_ram.sh $OPERATION
 
 # RANK OPERATION
-elif [ $operation == "rank" ]
+elif [ $OPERATION == "rank" ]
 then
-	/usr/bin/time -v -o "./result/rank/rank_operation_time.txt" ./operating_script/rank.sh $rank_number | \
-		./measure_ram.sh $operation
+	/usr/bin/time -v -o "./result/rank/rank_operation_time.txt" ./operating_script/rank.sh | \
+		./measure_ram.sh $OPERATION
 
 # BETWEEN OPERATION
-elif [ $operation == "between" ]
+elif [ $OPERATION == "between" ]
 then
 
 	/usr/bin/time -v -o "./result/between/between_operation_time.txt" ./operating_script/between.sh| \
-		./measure_ram.sh $operation
+		./measure_ram.sh $OPERATION
 		
 # UPDATE OPERATION
-elif [ $operation == "update" ]
+elif [ $OPERATION == "update" ]
 then
-	/usr/bin/time -v -o "./update/update_operation_time.txt" ./operating_script/update.sh | \
-		./measure_ram.sh $operation
+	/usr/bin/time -v -o "./result/update/update_operation_time.txt" ./operating_script/update.sh | \
+		./measure_ram.sh $OPERATION
 fi
