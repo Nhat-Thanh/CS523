9a095fdc137ec5223f6ef3589e92b0b4aa8a49bd
Nhat-Thanh <luonkebenban@gmail.com>
2021-05-10 18:05:52 +0700

    shell script for insert operation

--- Btree/operating_script/insert.sh
+++ Btree/operating_script/insert.sh
@@ -1,14 +1,12 @@
-data_file_path="./data/insert_data.txt"
-
-database_file_path="./database.db"                                               
-                                                                                                                                                                                                              n=1000;                                                                                   
-echo "inserting $n elements into Btree"                                                                    
-                                                                                                                  
-line=(`cat $data_file_path`)                                                
-i=0                                                                                                    
-while [ $i -lt "$n" ]                                                                                           
+DATA_FILE_PATH="./data/insert_data.txt"
+DATABASE_FILE_PATH="./database.db"
+N=1000
+echo "inserting $N elements into Btree"                                                                                                                  
+LINE=(`cat $DATA_FILE_PATH`)
+I=0
+while [ $I -lt "$N" ]
 do
-        sqlite3 "$database_file_path" "INSERT INTO Btree VALUES (${line[$i]});"
-        i=$(($i+1))                                                                                               
-done                                                                                                                   
+        sqlite3 "$DATABASE_FILE_PATH" "INSERT INTO Btree VALUES (${LINE[$I]});"
+        I=$(($I+1))
+done
 echo "finish insert operation"
