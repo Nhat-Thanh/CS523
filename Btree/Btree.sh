cab3dcae967ff934a2ebfbbae9510be04ca5410c
Nhat-Thanh <luonkebenban@gmail.com>
2021-05-12 08:12:59 +0700

    Main shell script file to run all operations

--- Btree/Btree.sh
+++ Btree/Btree.sh
@@ -13,31 +13,52 @@ fi
 # INSERT OPERATION
 if [ $OPERATION == "insert" ]
 then
-	/usr/bin/time -v -o ./result/insert/insert_operation_time.txt ./operating_script/insert.sh | \
-		./measure_ram.sh $OPERATION
+	I=1000
+	while [ $I -lt 9001 ]
+	do
+		/usr/bin/time -v -o ./result/insert/$I/insert_operation_time.txt bash ./operating_script/insert.sh $I | ./measure_ram.sh $OPERATION $I
+		I=$(($I+1000))
+	done
 
 # DELETE OPERATION
 elif [ $OPERATION == "delete" ]
 then
-	/usr/bin/time -v -o "./result/delete/delete_operation_time.txt" ./operating_script/delete.sh | \
-		./measure_ram.sh $OPERATION
+
+	I=1000
+	while [ $I -lt 9001 ]
+	do
+		/usr/bin/time -v -o ./result/delete/$I/delete_operation_time.txt bash ./operating_script/delete.sh | ./measure_ram.sh $OPERATION $I
+		I=$(($I+1000))
+	done
+	
 
 # RANK OPERATION
 elif [ $OPERATION == "rank" ]
 then
-	/usr/bin/time -v -o "./result/rank/rank_operation_time.txt" ./operating_script/rank.sh | \
-		./measure_ram.sh $OPERATION
-
+	I=1000
+	while [ $I -lt 9001 ]
+	do
+		/usr/bin/time -v -o ./result/rank/$I/rank_operation_time.txt bash ./operating_script/rank.sh $I | ./measure_ram.sh $OPERATION $I
+		I=$(($I+1000))
+	done
+	
 # BETWEEN OPERATION
 elif [ $OPERATION == "between" ]
 then
-
-	/usr/bin/time -v -o "./result/between/between_operation_time.txt" ./operating_script/between.sh| \
-		./measure_ram.sh $OPERATION
+	I=1000
+	while [ $I -lt 9001 ]
+	do
+		/usr/bin/time -v -o ./result/between/$I/between_operation_time.txt bash ./operating_script/between.sh $I | ./measure_ram.sh $OPERATION $I
+		I=$(($I+1000))
+	done
 		
 # UPDATE OPERATION
 elif [ $OPERATION == "update" ]
 then
-	/usr/bin/time -v -o "./result/update/update_operation_time.txt" ./operating_script/update.sh | \
-		./measure_ram.sh $OPERATION
+	I=1000
+	while [ $I -lt 9001 ]
+	do
+		/usr/bin/time -v -o ./result/update/$I/update_operation_time.txt bash ./operating_script/update.sh $I | ./measure_ram.sh $OPERATION $I
+		I=$(($I+1000))
+	done
 fi
