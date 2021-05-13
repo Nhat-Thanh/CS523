c25ebd91423acdb201b54e407bea01892fb1a4c4
Nhat-Thanh <luonkebenban@gmail.com>
2021-05-13 21:08:07 +0700

    shell script file for creating database

--- Btree/CreateDatabase.sh
+++ Btree/CreateDatabase.sh
@@ -1,20 +1,9 @@
-DATA_FILE_PATH="./data/data.txt"
-DATABASE_FILE_PATH="./database.db"
-
-N=1000000;
-echo "creating a table which has $n records"
-
-LINE=(`sed -n '1,1000000 p' $DATA_FILE_PATH`);
-I=0
+echo "creating a table"
 
 # sqlite3 $DATABASE_FILE_PATH "DROP TABLE Btree;"
 
-sqlite3 $DATABASE_FILE_PATH "CREATE TABLE Btree (NUMBER INTEGER PRIMARY KEY);"
-
-while [ $I -lt "$N" ]
-do
-	sqlite3 $DATABASE_FILE_PATH "INSERT INTO Btree VALUES (${LINE[$I]});";
-	I=$(($I+1))
-done
+sqlite3 ./database.db "CREATE TABLE Btree (NUMBER INTEGER PRIMARY KEY);"
+sqlite3 ./database.db < ./sql/CreateDatabase/CreateDatabase_1.sql
+sqlite3 ./database.db < ./sql/CreateDatabase/CreateDatabase_2.sql
 
 echo "finish creating database"
