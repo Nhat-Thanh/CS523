80c556d8c8558608d19bc15cc802b7c11361e43c
Nhat-Thanh <luonkebenban@gmail.com>
2021-05-13 21:11:43 +0700

    shell script for update operation

--- Btree/operating_script/update.sh
+++ Btree/operating_script/update.sh
@@ -1,17 +1,2 @@
-N=$1
-OLD_DATA_FILE_PATH="./data/$N/old_data_for_update_operation.txt"
-NEW_DATA_FILE_PATH="./data/$N/new_data_for_update_operation.txt"
-DATABASE_FILE_PATH="./database.db"
-#N=(`wc -l < $OLD_DATA_FILE_PATH`)
-
-echo "$N elements are updating"
-
-OLD=(`cat $OLD_DATA_FILE_PATH`)
-NEW=(`cat $NEW_DATA_FILE_PATH`)
-I=0
-while [ $I -lt "$N" ]
-do
-	sqlite3 $DATABASE_FILE_PATH "UPDATE Btree SET NUMBER = ${NEW[$I]} WHERE NUMBER = ${OLD[$I]};"
-	I=$(($I+1))	
-done
+sqlite3 ./database.db < ./sql/update/update.sql
 echo "finish update operation"
