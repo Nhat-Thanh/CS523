fa7568e85422513ef8fc83fe59f918f15f1afae8
Nhat-Thanh <luonkebenban@gmail.com>
2021-05-13 21:11:37 +0700

    shell script for between operation

--- Btree/operating_script/between.sh
+++ Btree/operating_script/between.sh
@@ -1,15 +1,2 @@
-N=$1
-DATA_FILE_PATH="./data/$N/between_data.txt"
-DATABASE_FILE_PATH="./database.db"
-#N=(`wc -l < $DATA_FILE_PATH`)
-
-LINE=(`cat $DATA_FILE_PATH`)
-I=0
-
-while [ $I -lt "$N" ]
-do
-	sqlite3 $DATABASE_FILE_PATH "SELECT COUNT(*) FROM Btree WHERE NUMBER BETWEEN ${LINE[$I]} AND ${LINE[$I + 1]};";
-	I=$(($I+2))
-done
-
+sqlite3 ./database.db < ./sql/between/between.sql
 echo "finish between operation"
