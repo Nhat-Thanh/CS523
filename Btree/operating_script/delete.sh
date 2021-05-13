4253970006b2b23eb781b3abd0d9f21ee34f2692
Nhat-Thanh <luonkebenban@gmail.com>
2021-05-13 21:11:26 +0700

    shell script for delete operation

--- Btree/operating_script/delete.sh
+++ Btree/operating_script/delete.sh
@@ -1,16 +1,2 @@
-N=$1
-DATA_FILE_PATH="./data/$N/delete_data.txt"
-DATABASE_FILE_PATH="./database.db"                                                                                  
-
-echo "deleting $N elements from Btree"
-
-LINE=(`cat $DATA_FILE_PATH`)
-I=0
-
-while [ $I -lt "$N" ]
-do                                                                                                         
-        sqlite3 $DATABASE_FILE_PATH "DELETE FROM Btree WHERE NUMBER = ${LINE[$I]};"
-        I=$(($I+1))
-done
-
+sqlite3 ./database.db < ./sql/delete/delete.sql
 echo "finish delete operation"
