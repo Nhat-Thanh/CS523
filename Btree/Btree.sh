d69aebbd58c789d56cf2362dcb4f15032ce8cdcb
Nhat-Thanh <luonkebenban@gmail.com>
2021-05-13 21:08:13 +0700

    Main shell script file to run all operations

--- Btree/Btree.sh
+++ Btree/Btree.sh
@@ -4,61 +4,58 @@ if [ $OPERATION != "insert" -a \
 	$OPERATION != "delete" -a \
 	$OPERATION != "rank" -a \
 	$OPERATION != "between" -a \
-	$OPERATION != "update" ]
+	$OPERATION != "update" -a \
+	$OPERATION != "all" ]
 then
 	echo "operation is invalid
-	insert, delete, rank, between, update are acceptable"
+	all, insert, delete, rank, between, update are acceptable"
 	exit 0
 fi
+
 # INSERT OPERATION
-if [ $OPERATION == "insert" ]
+if [ $OPERATION == "all" ]
 then
-	I=1000
-	while [ $I -lt 9001 ]
-	do
-		/usr/bin/time -v -o ./result/insert/$I/insert_operation_time.txt bash ./operating_script/insert.sh $I | bash ./measure_ram.sh $OPERATION $I
-		I=$(($I+1000))
-	done
+	/usr/bin/time -v -o ./result/insert/insert_operation_time.txt bash ./operating_script/insert.sh | bash ./measure_ram.sh insert
+	echo "finish insert"
+	
+	/usr/bin/time -v -o ./result/update/update_operation_time.txt bash ./operating_script/update.sh | bash ./measure_ram.sh update
+	echo "finish update"
+	
+	/usr/bin/time -v -o ./result/between/between_operation_time.txt bash ./operating_script/between.sh | bash ./measure_ram.sh between
+	echo "finish between"
+	
+	/usr/bin/time -v -o ./result/rank/rank_operation_time.txt bash ./operating_script/rank.sh | bash ./measure_ram.sh rank
+	echo "finish rank"
+	
+	/usr/bin/time -v -o ./result/delete/delete_operation_time.txt bash ./operating_script/delete.sh | bash ./measure_ram.sh delete
+	echo "finish delete"
+
+elif [ $OPERATION == "insert" ]
+then
+	/usr/bin/time -v -o ./result/insert/insert_operation_time.txt bash ./operating_script/insert.sh | bash ./measure_ram.sh insert
+	echo "finish insert"
 
 # DELETE OPERATION
 elif [ $OPERATION == "delete" ]
 then
-
-	I=1000
-	while [ $I -lt 9001 ]
-	do
-		/usr/bin/time -v -o ./result/delete/$I/delete_operation_time.txt bash ./operating_script/delete.sh $I | bash ./measure_ram.sh $OPERATION $I
-		I=$(($I+1000))
-	done
-	
+	/usr/bin/time -v -o ./result/delete/delete_operation_time.txt bash ./operating_script/delete.sh | bash ./measure_ram.sh delete
+	echo "finish delete"
 
 # RANK OPERATION
 elif [ $OPERATION == "rank" ]
 then
-	I=1000
-	while [ $I -lt 9001 ]
-	do
-		/usr/bin/time -v -o ./result/rank/$I/rank_operation_time.txt bash ./operating_script/rank.sh $I | bash ./measure_ram.sh $OPERATION $I
-		I=$(($I+1000))
-	done
-	
+	/usr/bin/time -v -o ./result/rank/rank_operation_time.txt bash ./operating_script/rank.sh | bash ./measure_ram.sh rank
+	echo "finish rank"
+
 # BETWEEN OPERATION
 elif [ $OPERATION == "between" ]
 then
-	I=1000
-	while [ $I -lt 9001 ]
-	do
-		/usr/bin/time -v -o ./result/between/$I/between_operation_time.txt bash ./operating_script/between.sh $I | bash ./measure_ram.sh $OPERATION $I
-		I=$(($I+1000))
-	done
+	/usr/bin/time -v -o ./result/between/between_operation_time.txt bash ./operating_script/between.sh | bash ./measure_ram.sh between
+	echo "finish between"
 		
 # UPDATE OPERATION
 elif [ $OPERATION == "update" ]
 then
-	I=1000
-	while [ $I -lt 9001 ]
-	do
-		/usr/bin/time -v -o ./result/update/$I/update_operation_time.txt bash ./operating_script/update.sh $I | bash ./measure_ram.sh $OPERATION $I
-		I=$(($I+1000))
-	done
+	/usr/bin/time -v -o ./result/update/update_operation_time.txt bash ./operating_script/update.sh | bash ./measure_ram.sh update
+	echo "finish update"
 fi
