4efc8900d26ef2571caddfb80a5327b7f4750992
Nhat-Thanh <luonkebenban@gmail.com>
2021-05-13 21:07:59 +0700

    shell script for measuring ram usage

--- Btree/measure_ram.sh
+++ Btree/measure_ram.sh
@@ -1,18 +1,15 @@
 OPERATION=$1
-N=$2
-SENSORS_PATH="./result/$OPERATION/$N/"$OPERATION"_sensors.txt"
+SENSORS_PATH="./result/$OPERATION/"$OPERATION"_sensors.txt"
 
-sensors >> "$SENSORS_PATH";
+sensors > "$SENSORS_PATH"
 
-echo "%MEM   RSS  SIZE    VSZ CMD" >> "$SENSORS_PATH";
+echo "%MEM   RSS  SIZE    VSZ" >> $SENSORS_PATH
 
-COMMAND="/usr/bin/time -v -o ./result/"$OPERATION"/"$N"/"$OPERATION"_operation_time.txt bash ./operating_script/"$OPERATION".sh $N"
+COMMAND="/usr/bin/time -v -o ./result/$OPERATION/"$OPERATION"_operation_time.txt bash ./operating_script/$OPERATION.sh"
 
 while [ `pgrep --full "$COMMAND"` ]
 do
-	# top -p$(pgrep --full "$command") >> $SENSORS_PATH
 	ps --pid $(pgrep sqlite3) --format pmem,rss,size,vsize --no-headers >> "$SENSORS_PATH"
 done
 
-echo "finish measuring ram"
-
+echo "finish measuring ram for $OPERATION operation"
