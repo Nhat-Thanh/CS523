63a2562069ac0be6e53d41d06784de41374773eb
Nhat-Thanh <luonkebenban@gmail.com>
2021-05-13 21:11:19 +0700

    shell script for rank operation

--- Btree/operating_script/rank.sh
+++ Btree/operating_script/rank.sh
@@ -1,15 +1,2 @@
-N=$1
-DATA_FILE_PATH="./data/$N/rank_data.txt"
-DATABASE_FILE_PATH="./database.db"
-#N=(`wc -l < $DATA_FILE_PATH`)
-
-echo "ranking $N elements"
-LINE=(`cat $DATA_FILE_PATH`)
-I=0
-
-while [ $I -lt "$N" ]
-do
-	sqlite3 $DATABASE_FILE_PATH "SELECT COUNT(*) FROM Btree WHERE NUMBER > ${LINE[$I]};"
-	I=$(($I+1))
-done
+sqlite3 ./database.db < ./sql/rank/rank.sql
 echo "finish rank operation"
