63203d7d80e271162b57c9f51b6e37a14ba46291
VuNguyenNhatThanh <56261442+Nhat-Thanh@users.noreply.github.com>
2021-05-14 15:53:12 +0700

    Update Btree.sh

--- Btree/Btree.sh
+++ Btree/Btree.sh
@@ -21,14 +21,14 @@ then
 	/usr/bin/time -v -o ./result/update/update_operation_time.txt bash ./operating_script/update.sh | bash ./measure_ram.sh update
 	echo "finish update"
 	
-	/usr/bin/time -v -o ./result/between/between_operation_time.txt bash ./operating_script/between.sh | bash ./measure_ram.sh between
-	echo "finish between"
+	# /usr/bin/time -v -o ./result/between/between_operation_time.txt bash ./operating_script/between.sh | bash ./measure_ram.sh between
+	# echo "finish between"
 	
-	/usr/bin/time -v -o ./result/rank/rank_operation_time.txt bash ./operating_script/rank.sh | bash ./measure_ram.sh rank
-	echo "finish rank"
+	# /usr/bin/time -v -o ./result/rank/rank_operation_time.txt bash ./operating_script/rank.sh | bash ./measure_ram.sh rank
+	# echo "finish rank"
 	
-	/usr/bin/time -v -o ./result/delete/delete_operation_time.txt bash ./operating_script/delete.sh | bash ./measure_ram.sh delete
-	echo "finish delete"
+	# /usr/bin/time -v -o ./result/delete/delete_operation_time.txt bash ./operating_script/delete.sh | bash ./measure_ram.sh delete
+	# echo "finish delete"
 
 elif [ $OPERATION == "insert" ]
 then
