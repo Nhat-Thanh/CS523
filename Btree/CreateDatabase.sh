9a7bf6e099cf7152b6c9b6f58b0b4d51f8f01b26
Nhat-Thanh <luonkebenban@gmail.com>
2021-05-15 02:51:01 +0700

    shell script file for creating database

--- Btree/CreateDatabase.sh
+++ Btree/CreateDatabase.sh
@@ -1,9 +1,67 @@
-echo "creating a table"
+SIZE=$1
 
-# sqlite3 $DATABASE_FILE_PATH "DROP TABLE Btree;"
+if [ $SIZE -eq 250000000 ]; then
+    echo "250 trieu"
+    sqlite3 ./database_250000000.db "CREATE TABLE Btree (NUMBER INTEGER PRIMARY KEY);"
+    sqlite3 ./database_250000000.db <./sql/CreateDatabase/250000000/CreateDatabase_1.sql
 
-sqlite3 ./database.db "CREATE TABLE Btree (NUMBER INTEGER PRIMARY KEY);"
-sqlite3 ./database.db < ./sql/CreateDatabase/CreateDatabase_1.sql
-sqlite3 ./database.db < ./sql/CreateDatabase/CreateDatabase_2.sql
+elif [ $SIZE -eq 500000000 ]; then
 
-echo "finish creating database"
+    echo "500 trieu"
+    sqlite3 ./database_500000000.db "CREATE TABLE Btree (NUMBER INTEGER PRIMARY KEY);"
+    sqlite3 ./database_500000000.db <./sql/CreateDatabase/500000000/CreateDatabase_1.sql
+
+elif [ $SIZE -eq 750000000 ]; then
+    echo "750 trieu"
+    sqlite3 ./database_750000000.db "CREATE TABLE Btree (NUMBER INTEGER PRIMARY KEY);"
+    sqlite3 ./database_750000000.db <./sql/CreateDatabase/750000000/CreateDatabase_1.sql
+    sqlite3 ./database_750000000.db <./sql/CreateDatabase/750000000/CreateDatabase_2.sql
+
+elif [ $SIZE -eq 1000000000 ]; then
+    echo "1000 trieu"
+    sqlite3 ./database_1000000000.db "CREATE TABLE Btree (NUMBER INTEGER PRIMARY KEY);"
+    sqlite3 ./database_1000000000.db <./sql/CreateDatabase/1000000000/CreateDatabase_1.sql
+    sqlite3 ./database_1000000000.db <./sql/CreateDatabase/1000000000/CreateDatabase_2.sql
+    sqlite3 ./database_1000000000.db <./sql/CreateDatabase/1000000000/CreateDatabase_2.sql
+    sqlite3 ./database_1000000000.db <./sql/CreateDatabase/1000000000/CreateDatabase_4.sql
+    sqlite3 ./database_1000000000.db <./sql/CreateDatabase/1000000000/CreateDatabase_5.sql
+
+elif [ $SIZE -eq 1250000000 ]; then
+    echo "1250 trieu"
+    sqlite3 ./database_1250000000.db "CREATE TABLE Btree (NUMBER INTEGER PRIMARY KEY);"
+    sqlite3 ./database_1250000000.db <./sql/CreateDatabase/1250000000/CreateDatabase_1.sql
+    sqlite3 ./database_1250000000.db <./sql/CreateDatabase/1250000000/CreateDatabase_2.sql
+    sqlite3 ./database_1250000000.db <./sql/CreateDatabase/1250000000/CreateDatabase_2.sql
+    sqlite3 ./database_1250000000.db <./sql/CreateDatabase/1250000000/CreateDatabase_4.sql
+    sqlite3 ./database_1250000000.db <./sql/CreateDatabase/1250000000/CreateDatabase_5.sql
+
+elif [ $SIZE -eq 1500000000 ]; then
+    echo "1500 trieu"
+    sqlite3 ./database_1500000000.db "CREATE TABLE Btree (NUMBER INTEGER PRIMARY KEY);"
+    sqlite3 ./database_1500000000.db <./sql/CreateDatabase/1500000000/CreateDatabase_1.sql
+    sqlite3 ./database_1500000000.db <./sql/CreateDatabase/1500000000/CreateDatabase_2.sql
+    sqlite3 ./database_1500000000.db <./sql/CreateDatabase/1500000000/CreateDatabase_2.sql
+    sqlite3 ./database_1500000000.db <./sql/CreateDatabase/1500000000/CreateDatabase_4.sql
+    sqlite3 ./database_1500000000.db <./sql/CreateDatabase/1500000000/CreateDatabase_5.sql
+
+elif [ $SIZE -eq 1750000000 ]; then
+    echo "1750 trieu"
+    sqlite3 ./database_1750000000.db "CREATE TABLE Btree (NUMBER INTEGER PRIMARY KEY);"
+    sqlite3 ./database_1750000000.db <./sql/CreateDatabase/1750000000/CreateDatabase_1.sql
+    sqlite3 ./database_1750000000.db <./sql/CreateDatabase/1750000000/CreateDatabase_2.sql
+    sqlite3 ./database_1750000000.db <./sql/CreateDatabase/1750000000/CreateDatabase_2.sql
+    sqlite3 ./database_1750000000.db <./sql/CreateDatabase/1750000000/CreateDatabase_4.sql
+    sqlite3 ./database_1750000000.db <./sql/CreateDatabase/1750000000/CreateDatabase_5.sql
+
+elif [ $SIZE -eq 2000000000 ]; then
+    echo "2000 trieu"
+    sqlite3 ./database_2000000000.db "CREATE TABLE Btree (NUMBER INTEGER PRIMARY KEY);"
+    sqlite3 ./database_2000000000.db <./sql/CreateDatabase/2000000000/CreateDatabase_1.sql
+    sqlite3 ./database_2000000000.db <./sql/CreateDatabase/2000000000/CreateDatabase_2.sql
+    sqlite3 ./database_2000000000.db <./sql/CreateDatabase/2000000000/CreateDatabase_2.sql
+    sqlite3 ./database_2000000000.db <./sql/CreateDatabase/2000000000/CreateDatabase_4.sql
+    sqlite3 ./database_2000000000.db <./sql/CreateDatabase/2000000000/CreateDatabase_5.sql
+fi
+
+echo "finish creating databases"
+exit 0
