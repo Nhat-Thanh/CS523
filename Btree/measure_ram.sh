4fbb8f9574c4eb859a7bb702b35226f1df78d1b0
Nhat-Thanh <luonkebenban@gmail.com>
2021-05-15 02:51:29 +0700

    shell script for measuring ram usage

--- Btree/measure_ram.sh
+++ Btree/measure_ram.sh
@@ -1,15 +1,19 @@
 OPERATION=$1
-SENSORS_PATH="./result/$OPERATION/"$OPERATION"_sensors.txt"
+SIZE=$2
+SENSORS_PATH="./result/$SIZE/$OPERATION/"$OPERATION"_sensors.txt"
+PATH="./result/$SIZE/$OPERATION/"$OPERATION"_disk.txt"
 
-sensors > "$SENSORS_PATH"
+sensors >"$SENSORS_PATH"
 
-echo "%MEM   RSS  SIZE    VSZ" >> $SENSORS_PATH
+echo "%MEM   RSS  SIZE    VSZ" >>$SENSORS_PATH
 
-COMMAND="/usr/bin/time -v -o ./result/$OPERATION/"$OPERATION"_operation_time.txt bash ./operating_script/$OPERATION.sh"
+COMMAND="/usr/bin/time -v -o ./result/$SIZE/$OPERATION/"$OPERATION"_operation_time.txt bash ./operating_script/$OPERATION.sh $SIZE"
 
-while [ `pgrep --full "$COMMAND"` ]
-do
-	ps --pid $(pgrep sqlite3) --format pmem,rss,size,vsize --no-headers >> "$SENSORS_PATH"
+while [ $(pgrep --full "$COMMAND") ]; do
+	ps --pid $(pgrep sqlite3) --format pmem,rss,size,vsize --no-headers >>"$SENSORS_PATH" |
+		du --summarize --separate-dirs --threshold=9K * && du --summarize >>$PATH
+	sleep 3
 done
 
-echo "finish measuring ram for $OPERATION operation"
+echo "finish measuring ram and disk for $OPERATION operation"
+exit 0
