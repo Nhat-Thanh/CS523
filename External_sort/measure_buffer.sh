52dfd5fdbd47613fdb9b58d7ce4cdee4061a69aa
Nhat-Thanh <luonkebenban@gmail.com>
2021-05-24 21:34:30 +0700

    sort with linear ascent buffers

---
+++ External_sort/measure_buffer.sh
@@ -0,0 +1,81 @@
+./sort.sh 500  4 20M buffer
+
+sleep 150
+
+./sort.sh 500  4 60M buffer
+
+sleep 140
+
+./sort.sh 500 4 100M buffer
+
+sleep 120
+
+./sort.sh 500 4 150M buffer
+
+sleep 110
+
+./sort.sh 500 4 200M buffer
+
+sleep 100
+
+./sort.sh 500 4 250M buffer
+
+sleep 90
+
+./sort.sh 500 4 300M buffer
+
+sleep 80
+
+./sort.sh 500 4 350M buffer
+
+sleep 80
+
+./sort.sh 500 4 400M buffer
+
+sleep 80
+
+./sort.sh 500 4 450M buffer
+
+sleep 80
+
+./sort.sh 500 4 500M buffer
+
+sleep 80
+
+./sort.sh 500 4 550M buffer
+
+sleep 80
+
+./sort.sh 500 4 600M buffer
+
+sleep 80
+
+./sort.sh 500 4 650M buffer
+
+sleep 80
+
+./sort.sh 500 4 700M buffer
+
+sleep 80
+
+./sort.sh 500 4 750M buffer
+
+sleep 80
+
+./sort.sh 500 4 800M buffer
+
+sleep 80
+
+./sort.sh 500 4 850M buffer
+
+sleep 80
+
+./sort.sh 500 4 900M buffer
+
+sleep 80
+
+./sort.sh 500 4 950M buffer
+
+sleep 80
+
+./sort.sh 500  4 1G buffer
