90178d40550d552ff93932428101fe19c4017629
Nhat-Thanh <luonkebenban@gmail.com>
2021-05-24 21:35:33 +0700

    sort with linear ascent cores

---
+++ External_sort/measure_parallel.sh
@@ -0,0 +1,29 @@
+./sort.sh 500  1 100M parallel
+
+sleep 210
+
+./sort.sh 500  2 100M parallel
+
+sleep 200
+
+./sort.sh 500  3 100M parallel
+
+sleep 190
+
+./sort.sh 500  4 100M parallel
+
+sleep 180
+
+./sort.sh 500  5 100M parallel
+
+sleep 170
+
+./sort.sh 500  6 100M parallel
+
+sleep 160
+
+./sort.sh 500  7 100M parallel
+
+sleep 150
+
+./sort.sh 500  8 100M parallel
