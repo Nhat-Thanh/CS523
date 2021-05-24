e8a82c792b615afbb8e3b2e987266689c0cd1151
Nhat-Thanh <luonkebenban@gmail.com>
2021-05-24 21:35:06 +0700

    sort with linear ascent SIZE

---
+++ External_sort/measure_complexity.sh
@@ -0,0 +1,149 @@
+./sort.sh 3 1 10M complexity
+
+sleep 30
+
+./sort.sh 7 1 10M complexity
+
+sleep 40
+
+./sort.sh 10 1 10M complexity
+
+sleep 50
+
+./sort.sh 14 1 10M complexity
+
+sleep 50
+
+./sort.sh 20 1 10M complexity
+
+sleep 60
+
+./sort.sh 25 1 10M complexity
+
+sleep 60
+
+./sort.sh 30 1 10M complexity
+
+sleep 60
+
+./sort.sh 35 1 10M complexity
+
+sleep 60
+
+./sort.sh 40 1 10M complexity
+
+sleep 70
+
+./sort.sh 45 1 10M complexity
+
+sleep 70
+
+./sort.sh 50 1 10M complexity
+
+sleep 70
+
+./sort.sh 55 1 10M complexity
+
+sleep 70
+
+./sort.sh 60 1 10M complexity
+
+sleep 70
+
+./sort.sh 65 1 10M complexity
+
+sleep 80
+
+./sort.sh 70 1 10M complexity
+
+sleep 90
+
+./sort.sh 75 1 10M complexity
+
+sleep 90
+
+./sort.sh 80 1 10M complexity
+
+sleep 90
+
+./sort.sh 85 1 10M complexity
+
+sleep 100
+
+./sort.sh 90 1 10M complexity
+
+sleep 110
+
+./sort.sh 100 1 10M complexity
+
+sleep 110
+
+./sort.sh 150 1 10M complexity
+
+sleep 110
+
+./sort.sh 200 1 10M complexity
+
+sleep 120
+
+./sort.sh 250 1 10M complexity
+
+sleep 130
+
+./sort.sh 300 1 10M complexity
+
+sleep 130
+
+./sort.sh 350 1 10M complexity
+
+sleep 130
+
+./sort.sh 400 1 10M complexity
+
+sleep 140
+
+./sort.sh 450 1 10M complexity
+
+sleep 150
+
+./sort.sh 500 1 10M complexity
+
+sleep 150
+
+./sort.sh 550 1 10M complexity
+
+sleep 150
+
+./sort.sh 600 1 10M complexity
+
+sleep 160
+
+./sort.sh 650 1 10M complexity
+
+sleep 170
+
+./sort.sh 700 1 10M complexity
+
+sleep 170
+
+./sort.sh 750 1 10M complexity
+
+sleep 170
+
+./sort.sh 800 1 10M complexity
+
+sleep 180
+
+./sort.sh 850 1 10M complexity
+
+sleep 190
+
+./sort.sh 900 1 10M complexity
+
+sleep 190
+
+./sort.sh 950 1 10M complexity
+
+sleep 190
+
+./sort.sh 1000 1 10M complexity
