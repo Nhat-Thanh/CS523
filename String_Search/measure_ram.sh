91c4db02f47b7760ad40a2759d4e7a8c475cf813
Nhat-Thanh <luonkebenban@gmail.com>
2021-06-06 10:37:11 +0700

    measure Ram and disk usage

---
+++ String_Search/measure_ram.sh
@@ -0,0 +1,21 @@
+TEST_NAME=$1
+TH=$2
+PARAM=$3
+PARAM_SIZE=$4
+
+SENSORS_PATH="./result/$TH/$PARAM/$PARAM_SIZE/"$TEST_NAME"_sensors.txt"
+DISK_PATH="./result/$TH/$PARAM/$PARAM_SIZE/"$TEST_NAME"_disk.txt"
+
+sensors >$SENSORS_PATH
+echo "" >$DISK_PATH
+echo "%MEM   RSS  SIZE    VSZ" >>$SENSORS_PATH
+
+COMMAND="/usr/bin/time -v -o ./result/$TH/$PARAM/$PARAM_SIZE/$TEST_NAME.txt bash ./script/$PARAM.sh $TEST_NAME $TH $PARAM_SIZE"
+
+while [ $(pgrep --full "$COMMAND") ]; do
+	ps --pid $(pgrep grep) --format pmem,rss,size,vsize --no-headers >>"$SENSORS_PATH" |
+		du --summarize /tmp >>$DISK_PATH
+done
+
+echo "finish measuring ram and disk for $TEST_NAME.txt - $TH - $PARAM - $PARAM_SIZE"
+exit 0
