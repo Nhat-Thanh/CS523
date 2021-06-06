d68cf307869ade8de205a65d39bead88c958b0cc
Nhat-Thanh <luonkebenban@gmail.com>
2021-06-06 10:40:31 +0700

    init folders in result

--- String_Search/init.sh
+++ String_Search/init.sh
@@ -1,15 +1,15 @@
 i=0
 
-mkdir ./result/TH1/;
-mkdir ./result/TH2/;
-mkdir ./result/TH1/n/;
-mkdir ./result/TH1/k/;
-mkdir ./result/TH2/n/;
+mkdir -p ./result/TH1/;
+mkdir -p ./result/TH2/;
+mkdir -p ./result/TH1/n/;
+mkdir -p ./result/TH1/k/;
+mkdir -p ./result/TH2/n/;
 
 while [ $i -lt 100 ]
 do
-	mkdir ./result/TH1/n/$((505000 + 5000 * $i));
-	mkdir ./result/TH1/k/$((100 + 100 * $i));
-	mkdir ./result/TH2/n/$((10000000 + 10000000 * $i));
+	mkdir -p ./result/TH1/n/$((505000 + 5000 * $i));
+	mkdir -p ./result/TH1/k/$((100 + 100 * $i));
+	mkdir -p ./result/TH2/n/$((10000000 + 10000000 * $i));
 	i=$(($i + 1));
 done
