556db0cb16790535c659189ab9709d511e0b5f23
Nhat-Thanh <luonkebenban@gmail.com>
2021-06-06 11:50:49 +0700

    main shell script

--- String_Search/grep.sh
+++ String_Search/grep.sh
@@ -1,10 +1,11 @@
 TEST_NAME=$1
 TH=$2
 PARAM=$3
+NUM_FILE=$4
 i=0
 
 if [ "$PARAM" == "n" -a "$TH" == "TH1" ]; then
-    while [ $i -lt 100 ]; do
+    while [ $i -lt "$NUM_FILE" ]; do
         SIZE=$((505000 + 5000 * $i))
 
         /usr/bin/time -v -o ./result/TH1/n/$SIZE/$TEST_NAME.txt bash ./script/n.sh $TEST_NAME TH1 $SIZE |
@@ -14,7 +15,7 @@ if [ "$PARAM" == "n" -a "$TH" == "TH1" ]; then
     done
 
 elif [ "$PARAM" == "n" -a "$TH" == "TH2" ]; then
-    while [ $i -lt 100 ]; do
+    while [ $i -lt "$NUM_FILE" ]; do
         SIZE=$((10000000 + 10000000 * $i))
 
         /usr/bin/time -v -o ./result/TH2/n/$SIZE/$TEST_NAME.txt bash ./script/n.sh $TEST_NAME TH2 $SIZE |
@@ -24,7 +25,7 @@ elif [ "$PARAM" == "n" -a "$TH" == "TH2" ]; then
     done
 
 elif [ "$PARAM" == "k" -a "$TH" == "TH1" ]; then
-    while [ $i -lt 100 ]; do
+    while [ $i -lt "$NUM_FILE" ]; do
         SIZE=$((100 + 100 * $i))
 
         /usr/bin/time -v -o ./result/TH1/k/$SIZE/$TEST_NAME.txt bash ./script/k.sh $TEST_NAME TH1 $SIZE |
