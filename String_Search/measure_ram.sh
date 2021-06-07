f1a4a0700b5a6d9ac658b92f6a8fdd8f82b97bb5
Nhat-Thanh <luonkebenban@gmail.com>
2021-06-07 22:11:10 +0700

    measure Ram and disk usage

--- String_Search/measure_ram.sh
+++ String_Search/measure_ram.sh
@@ -10,16 +10,15 @@ sensors >$SENSORS_PATH
 echo "" >$DISK_PATH
 echo "%MEM   RSS  SIZE    VSZ" >>$SENSORS_PATH
 
-#COMMAND="/usr/bin/time -v -o ./result/$TH/$PARAM/$PARAM_SIZE/$TEST_NAME.txt bash ./script/$PARAM.sh $TEST_NAME $TH $PARAM_SIZE"
+COMMAND="/usr/bin/time -v -o ./result/$TH/$PARAM/$PARAM_SIZE/$TEST_NAME.txt bash ./script/$PARAM.sh $TEST_NAME $TH $PARAM_SIZE"
 
 PARENT_COMMAND="bash ./script/$PARAM.sh $TEST_NAME $TH $PARAM_SIZE"
 
-GREP_PID=$(ps --ppid $(pgrep -x -f "$PARENT_COMMAND") --format pid --no-headers)
+# GREP_PID=$(ps --ppid $(pgrep -x -f "$PARENT_COMMAND") --format pid --no-headers)
 
-while [ $(pgrep -x -f "$PARENT_COMMAND") ]; do
-	ps --pid $GREP_PID --format pmem,rss,size,vsize --no-headers >>"$SENSORS_PATH" |
+while [ $(pgrep --full "$COMMAND") ]; do
+	ps --pid $(ps --ppid $(pgrep -x -f "$PARENT_COMMAND") --format pid --no-headers) --format pmem,rss,size,vsize --no-headers >>"$SENSORS_PATH" |
 		du --summarize /tmp >>$DISK_PATH
 done
-
 echo "finish measuring ram and disk for $TEST_NAME.txt - $TH - $PARAM - $PARAM_SIZE"
 exit 0
