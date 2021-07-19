601d7fe8087fa0f26621ca8e9922375a6c25f452
VuNguyenNhatThanh <56261442+Nhat-Thanh@users.noreply.github.com>
2021-07-19 08:39:51 +0700

    Delete 4000.sh

--- External_sort/4000.sh
+++
@@ -1,1 +0,0 @@
-/usr/bin/time -v -o ./time_output/complexity/Test_4000_trieu/time_output_4000_trieu sort -n ./input/input_4000_trieu.txt -o ./output/output_4000_trieu.txt | ./meature_ram.sh 4000 noCores noBuffer default
